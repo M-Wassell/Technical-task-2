44ffb95996bd8af240bc8b75b1ce78fbb8ebf7b2
M-Wassell <55836302+M-Wassell@users.noreply.github.com>
2021-06-17 21:25:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+'use strict'
+
+// function SnapFunction()
+// {
+//     var x, y;
+
+//     x=document.getElementById("form1") ;
+//     y=x.elements["userIn"].value;
+//     document.getElementById("output").value = y;
+//     document.getElementById("output").innerHTML+=y;
+//     event.preventDefault();//wont let the page refresh, if not used answer flashes once on screen.
+// }
+    
+// function printRange(rangeStart, rangeStop)
+//  {
+//     var text = "";
+//     for (var i = rangeStart; i <= rangeStop; i++) 
+//     {
+//           text += i + ',';
+//         }
+      
+//         return text.slice(0, -1);
+//       }
+      
+//       document.getElementById("output").value = y;
+
+    
+
+
